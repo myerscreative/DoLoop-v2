cda3d604523ffb604af13b32a694d86b7545a7c3
Robert Myers <myersgroup@gmail.com>
2025-11-05 09:34:21 -0800

    Replace bee illustration on create loop page with new DoLoop bee SVG

--- src/app/loops/create/page.tsx
+++ src/app/loops/create/page.tsx
@@ -19,67 +19,12 @@ export default function CreateLoopWelcomePage() {
         transition={{ duration: 0.5, type: 'spring' }}
         className="mb-8"
       >
-        <svg
-          width="120"
-          height="120"
-          viewBox="0 0 120 120"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          {/* Bee Head */}
-          <circle cx="60" cy="40" r="28" fill="#FFB800" />
-          {/* Eyes */}
-          <circle cx="52" cy="36" r="6" fill="white" />
-          <circle cx="68" cy="36" r="6" fill="white" />
-          <circle cx="52" cy="36" r="3" fill="black" />
-          <circle cx="68" cy="36" r="3" fill="black" />
-          {/* Smile */}
-          <path
-            d="M 50 48 Q 60 52, 70 48"
-            stroke="black"
-            strokeWidth="2"
-            fill="none"
-            strokeLinecap="round"
-          />
-          {/* Antennae */}
-          <circle cx="50" cy="20" r="2" fill="black" />
-          <circle cx="70" cy="20" r="2" fill="black" />
-          <path
-            d="M 50 20 Q 48 16, 50 12"
-            stroke="black"
-            strokeWidth="2"
-            fill="none"
-          />
-          <path
-            d="M 70 20 Q 72 16, 70 12"
-            stroke="black"
-            strokeWidth="2"
-            fill="none"
-          />
-          {/* Bee Body */}
-          <ellipse cx="60" cy="75" rx="22" ry="32" fill="black" />
-          {/* Yellow Stripes */}
-          <ellipse cx="60" cy="65" rx="20" ry="6" fill="#FFB800" />
-          <ellipse cx="60" cy="75" rx="20" ry="6" fill="#FFB800" />
-          <ellipse cx="60" cy="85" rx="20" ry="6" fill="#FFB800" />
-          {/* Wings */}
-          <ellipse
-            cx="45"
-            cy="70"
-            rx="12"
-            ry="18"
-            fill="#90CAF9"
-            opacity="0.6"
-          />
-          <ellipse
-            cx="75"
-            cy="70"
-            rx="12"
-            ry="18"
-            fill="#90CAF9"
-            opacity="0.6"
-          />
-        </svg>
+        <img
+          src="/doloop-bee.svg"
+          alt="DoLoop Bee"
+          width="200"
+          height="200"
+        />
       </motion.div>
 
       {/* Title */}
